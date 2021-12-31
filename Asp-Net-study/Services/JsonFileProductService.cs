36894495f296b4826782f5a0555d47c01b2ae3d7
YUN <yunmyeonghun@gmail.com>
2021-12-31 18:17:51 +0900

    study 02

---
+++ Asp-Net-study/Services/JsonFileProductService.cs
@@ -0,0 +1,32 @@
+﻿using Asp_Net_study.Models;
+using System.Text.Json;
+
+namespace Asp_Net_study.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
